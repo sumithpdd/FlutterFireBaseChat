1b02cd732f834f81578db5efbe365c9ea961efd3
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-03-21 12:20:43 +0100

    added provider and userdata

--- Code/flutter_firebase_chat/lib/main.dart
+++ Code/flutter_firebase_chat/lib/main.dart
@@ -3,11 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_chat/screens/home_screen.dart';
 import 'package:flutter_firebase_chat/screens/login_screen.dart';
 import 'package:flutter_firebase_chat/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+import 'models/user_data.dart';
+
+void main() => runApp(MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (_) => UserData(),
+      ),
+      Provider<AuthService>(
+        create: (_) => AuthService(),
+      )
+    ], child: MyApp()));
 
 class MyApp extends StatelessWidget {
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Firebase Chat',
@@ -16,9 +26,11 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
       ),
       home: StreamBuilder<FirebaseUser>(
-        stream: AuthService().user,
+        stream: Provider.of<AuthService>(context, listen: false).user,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
+            Provider.of<UserData>(context, listen: false).currentUserId =
+                snapshot.data.uid;
             return HomeScreen();
           } else {
             return LoginScreen();
